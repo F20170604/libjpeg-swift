792567b24e805a42c2b8220840c25d30a1c16235
Ayush Agrawal <f20170604@goa.bits-pilani.ac.in>
2020-06-14 05:11:44 +0530

    get size

--- Sources/libjpeg/include/JPEGImageOps.h
+++ Sources/libjpeg/include/JPEGImageOps.h
@@ -14,12 +14,12 @@
 
 DLLEXPORT unsigned char *tjJPEGLoadImage(const char *filename, int *width,
                                          int align, int *height, int *pixelFormat, int *inSubsamp,
-                                         int flags);
+                                         long *size, int flags);
 
 DLLEXPORT unsigned char* tjJPEGSaveImage(unsigned char *buffer,
                                          int width, int pitch, int height, int pixelFormat, int outSubsamp,
-                                         int flags);
+                                         long size, int flags);
 
 DLLEXPORT unsigned char* tjJPEGLoadImageCompressed(const char *filename, int *width,
                                                    int align, int *height, int *pixelFormat, int *inSubsamp,
-                                                   int flags);
+                                                   long *size, int flags);
