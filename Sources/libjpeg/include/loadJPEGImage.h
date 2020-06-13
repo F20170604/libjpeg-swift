ab1b6a02c1a483de0b62fd6fbde55239f7485063
Ayush Agrawal <f20170604@goa.bits-pilani.ac.in>
2020-06-13 18:34:03 +0530

    update

---
+++ Sources/libjpeg/include/loadJPEGImage.h
@@ -0,0 +1,17 @@
+//
+//  File.h
+//  
+//
+//  Created by Ayush Agrawal on 13/06/20.
+//
+
+#ifndef loadJPEGImage_h
+#define loadJPEGImage_h
+
+#include <stdio.h>
+
+#endif /* loadJPEGImage_h */
+
+DLLEXPORT unsigned char *tjJPEGLoadImage(const char *filename, int *width,
+                                         int align, int *height, int *pixelFormat,
+                                         int flags);
