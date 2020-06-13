d3cc5709be7110f25bfbe04ccc4a8d7687f248cd
Ayush Agrawal <f20170604@goa.bits-pilani.ac.in>
2020-06-14 05:02:56 +0530

    update

--- Sources/libjpeg/include/JPEGImageOps.h
+++ Sources/libjpeg/include/JPEGImageOps.h
@@ -16,9 +16,9 @@ DLLEXPORT unsigned char *tjJPEGLoadImage(const char *filename, int *width,
                                          int align, int *height, int *pixelFormat, int *inSubsamp,
                                          int flags);
 
-DLLEXPORT int tjJPEGSaveImage(const char *filename, unsigned char *buffer,
-                              int width, int pitch, int height, int pixelFormat, int outSubsamp,
-                              int flags);
+DLLEXPORT unsigned char* tjJPEGSaveImage(unsigned char *buffer,
+                                         int width, int pitch, int height, int pixelFormat, int outSubsamp,
+                                         int flags);
 
 DLLEXPORT unsigned char* tjJPEGLoadImageCompressed(const char *filename, int *width,
                                                    int align, int *height, int *pixelFormat, int *inSubsamp,
